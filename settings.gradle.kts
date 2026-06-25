79fca2f85ece99b0b9ec46f46eaa71dab73d75b8
5pap <patrick@seven24.ca>
2026-06-25 14:35:10 -0400

    Fix settings.gradle.kts: use maven() function call syntax

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
     repositories {
         mavenCentral()
-        maven { url "https://maven.fabricmc.net/" }
+        maven("https://maven.fabricmc.net/")
     }
 }
 rootProject.name = "croppie_counter"
